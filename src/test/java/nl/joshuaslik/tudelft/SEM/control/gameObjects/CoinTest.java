27dcdfad858465bca2ededc35a4d01c73120a6d6
mhoutman <m.houtman@student.tudelft.nl>
2015-10-09 22:35:50 +0200

    add CoinTest, LifeTest

--- src/test/java/nl/joshuaslik/tudelft/SEM/control/gameObjects/CoinTest.java
+++ src/test/java/nl/joshuaslik/tudelft/SEM/control/gameObjects/CoinTest.java
@@ -1,22 +1,45 @@
 package nl.joshuaslik.tudelft.SEM.control.gameObjects;
 
-import static org.junit.Assert.*;
 import nl.joshuaslik.tudelft.SEM.control.gameObjects.pickup.Coin;
+import nl.joshuaslik.tudelft.SEM.control.viewController.viewObjects.ImageViewObject;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
 import static org.mockito.Mockito.verify;
 
+import java.io.InputStream;
+
+@RunWith(MockitoJUnitRunner.class) 
 public class CoinTest {
 
-	GameObjects go = Mockito.mock(GameObjects.class);
+	@Mock
+	GameObjects go;
 	
+	@Mock
+	ImageViewObject image;
+	
+	Coin coin;
+	
+	@Before
+	public void setup() {
+		Mockito.when(go.makeImage(Mockito.any(InputStream.class), Mockito.anyDouble(), Mockito.anyDouble())).thenReturn(image);
+		Mockito.when(go.getLeftBorder()).thenReturn(0.0);
+		Mockito.when(go.getRightBorder()).thenReturn(100.0);
+		Mockito.when(go.getTopBorder()).thenReturn(0.0);
+		Mockito.when(go.getBottomBorder()).thenReturn(100.0);
+		coin = new Coin(go, 400, 400);
+	}
 	@Test
 	public void testHandlePlayerCollision() {
-		
-		Coin coin = new Coin(go, 400, 400);
-		coin.handlePlayerCollision();
-		verify(coin).destroy();
+		Coin spyCoin = Mockito.spy(coin);
+		spyCoin.handlePlayerCollision();
+		verify(spyCoin).destroy();
+		verify(go).addPoints(10);
 	}
 
 }
--- src/test/java/nl/joshuaslik/tudelft/SEM/control/gameObjects/LifeTest.java
+++ src/test/java/nl/joshuaslik/tudelft/SEM/control/gameObjects/LifeTest.java
@@ -2,19 +2,45 @@ package nl.joshuaslik.tudelft.SEM.control.gameObjects;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.verify;
-import nl.joshuaslik.tudelft.SEM.control.gameObjects.pickup.Life;
-import org.junit.Test;
-import org.mockito.Mockito;
 
+import java.io.InputStream;
+
+import nl.joshuaslik.tudelft.SEM.control.gameObjects.pickup.Life;
+import nl.joshuaslik.tudelft.SEM.control.viewController.viewObjects.ImageViewObject;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class) 
 public class LifeTest {
 
-	GameObjects go = Mockito.mock(GameObjects.class);
-	Life life = new Life(go, 400, 400);
+	@Mock
+	GameObjects go;
+	
+	@Mock
+	ImageViewObject image;
+	
+	Life life;
+	@Before
+	public void setup() {
+		Mockito.when(go.makeImage(Mockito.any(InputStream.class), Mockito.anyDouble(), Mockito.anyDouble())).thenReturn(image);
+		Mockito.when(go.getLeftBorder()).thenReturn(0.0);
+		Mockito.when(go.getRightBorder()).thenReturn(100.0);
+		Mockito.when(go.getTopBorder()).thenReturn(0.0);
+		Mockito.when(go.getBottomBorder()).thenReturn(100.0);
+		life = new Life(go, 400, 400);
+	}
 	
 	@Test
 	public void testHandlePlayerCollision() {
-		verify(life).destroy();
-		verify(life).getGameObjects().addLife();;
+		Life spyLife = Mockito.spy(life);
+		spyLife.handlePlayerCollision();
+		verify(spyLife).destroy();
+		verify(go).addLife();
 	}
 
 }
