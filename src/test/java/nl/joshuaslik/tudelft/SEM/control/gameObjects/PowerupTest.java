7856e1b72a160e8b45727f265582946d305a2b7e
unknown <f.m.a.a.elghlan@student.tudelft.nl>
2015-10-09 21:01:52 +0200

    Fixed test <testing

--- src/test/java/nl/joshuaslik/tudelft/SEM/control/gameObjects/PowerupTest.java
+++ src/test/java/nl/joshuaslik/tudelft/SEM/control/gameObjects/PowerupTest.java
@@ -1,23 +1,50 @@
 package nl.joshuaslik.tudelft.SEM.control.gameObjects;
 
-import static org.junit.Assert.*;
+import java.io.InputStream;
+import static javafx.scene.input.KeyCode.M;
 import static org.mockito.Mockito.verify;
 import nl.joshuaslik.tudelft.SEM.control.gameObjects.pickup.powerup.EnumPowerupTypes;
 import nl.joshuaslik.tudelft.SEM.control.gameObjects.pickup.powerup.Powerup;
+import nl.joshuaslik.tudelft.SEM.control.viewController.viewObjects.IImageViewObject;
+import org.junit.Before;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PowerupTest {
 
-	IGameObjects igo = Mockito.mock(IGameObjects.class);
-	EnumPowerupTypes ept = Mockito.mock(EnumPowerupTypes.class);
-	Powerup powerup = new Powerup(igo, ept, 400, 400);
+        @Mock
+	private IGameObjects igo;
+        
+        @Mock
+	private IImageViewObject image;
+        
+	private Powerup powerup;
 	
+        /**
+         * Initialize and mock objects.
+         */
+        @Before
+        public void initialize() {
+            when(igo.getLeftBorder()).thenReturn(0.0);
+            when(igo.getRightBorder()).thenReturn(10.0);
+            when(igo.getBottomBorder()).thenReturn(10.0);
+            when(igo.getTopBorder()).thenReturn(0.0);
+            when(igo.makeImage(Mockito.any(InputStream.class), Mockito.any(Double.class), 
+                    Mockito.any(Double.class))).thenReturn(image);
+            
+            powerup = new Powerup(igo, EnumPowerupTypes.PLAYER_SPEED_UP, 400, 400);
+        }
+        
 	@Test
 	public void testHandlePlayerCollision() {
-		powerup.handlePlayerCollision();
-		verify(powerup).destroy();
+                Powerup spy = Mockito.spy(powerup);
+		spy.handlePlayerCollision();
+		verify(spy).destroy();
 	}
-
 }
