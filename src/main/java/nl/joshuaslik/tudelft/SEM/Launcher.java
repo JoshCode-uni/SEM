fb416eb24481d1e25089252cc27de115d014c0ad
JoshCode <joshua.b.slik@gmail.com>
2015-10-09 19:39:00 +0200

    Fix Checkstyle errors in Launcher
    <organisation

--- src/main/java/nl/joshuaslik/tudelft/SEM/Launcher.java
+++ src/main/java/nl/joshuaslik/tudelft/SEM/Launcher.java
@@ -33,18 +33,18 @@ public class Launcher extends Application {
     public static final int SCREEN_HEIGHT = 600;
     public static final double GRAVITY = 700;
     public static final double ENERGY = GRAVITY * SCREEN_HEIGHT; // E = .5v2 + gh
-    private static final BorderPane bp = new BorderPane();
+    private static final BorderPane BP = new BorderPane();
     private static Stage stage;
 
     /**
      * Start up the game.
      */
     @Override
-    public void start(Stage primaryStage) {
+    public final void start(final Stage primaryStage) {
         GameLog.constructor();
         loadView(getClass().getResource("/data/gui/pages/MainMenu.fxml"));
 
-        Scene scene = new Scene(bp);
+        Scene scene = new Scene(BP);
         primaryStage.setScene(scene);
         primaryStage.setFullScreen(true);
         primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
@@ -58,15 +58,15 @@ public class Launcher extends Application {
      * @param fxmlURL URL of the FXML file.
      * @return the controller of the loaded view.
      */
-    public static IviewController loadView(URL fxmlURL) {
+    public static IviewController loadView(final URL fxmlURL) {
         GameLog.addInfoLog("Load view: " + fxmlURL.getPath());
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(fxmlURL);
         try {
             Pane pane = loader.load();
             IviewController res = ((IviewController) loader.getController());
-            res.start(bp.getScene());
-            bp.setCenter(pane);
+            res.start(BP.getScene());
+            BP.setCenter(pane);
             return res;
         } catch (IOException ex) {
             GameLog.addErrorLog("Failed to load fxml file: " + fxmlURL.toString());
@@ -82,7 +82,7 @@ public class Launcher extends Application {
      * @param mainViewController the controller of the current view.
      * @param fxmlURL            the URL of the FXML file of the popup.
      */
-    public static void loadPopup(IviewController mainViewController, URL fxmlURL) {
+    public static void loadPopup(final IviewController mainViewController, final URL fxmlURL) {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(fxmlURL);
         mainViewController.setButtonsDisabled(true);
@@ -104,7 +104,7 @@ public class Launcher extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         launch(args);
     }
 }
