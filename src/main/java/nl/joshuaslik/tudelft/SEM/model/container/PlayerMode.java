d3f1ad6e9fc55f472c085ffa8fbf53498a6abaf7
unknown <f.m.a.a.elghlan@student.tudelft.nl>
2015-10-15 22:43:07 +0200

    Created a singleton to store game info. <feature

---
+++ src/main/java/nl/joshuaslik/tudelft/SEM/model/container/PlayerMode.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.joshuaslik.tudelft.SEM.model.container;
+
+/**
+ *
+ * @author Faris
+ */
+public enum PlayerMode {
+    SINGLE_PLAYER, MULTI_PLAYER_COOP, MULTI_PLAYER_VERSUS;
+}
